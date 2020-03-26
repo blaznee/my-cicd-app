7d76150ec507efd92cadf2efc2e1e7050e71cbd4
blaznee <62694552+blaznee@users.noreply.github.com>
2020-03-26 10:27:37 +0100

    Add files via upload

---
+++ test_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import unittest
+import app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_hello(self):
+        rv = self.app.get('/hello/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_name(self):
+        name = 'Simon'
+        rv = self.app.get('/hello/{name}')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn('{name}', rv.data)
+
+if __name__ == '__main__':
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
+    unittest.main()
